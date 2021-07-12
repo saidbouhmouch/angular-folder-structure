12144661314d56981945437270ed3ef45c932dfe
Said Bouhmouch <said.dev.info@gmail.com>
2021-07-12 18:00:55 +0100

    add home

---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit , OnDestroy{
+ 
+  constructor() { }
+
+  ngOnInit() { }
+
+  ngOnDestroy(): void { }
+}
